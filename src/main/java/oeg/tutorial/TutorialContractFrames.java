57a4e5a8ab3be70560eef10461debd61929bedd9
Victor Rodriguez Doncel <victorr@ac.upc.edu>
2018-09-07 11:27:53 +0200

    comentarios

--- src/main/java/oeg/tutorial/TutorialContractFrames.java
+++ src/main/java/oeg/tutorial/TutorialContractFrames.java
@@ -4,7 +4,7 @@ import oeg.contractFrames.ContractFrames;
 
 
 /**
- *
+ * Demonstrator class.
  * @author mnavas
  */
 public class TutorialContractFrames {
@@ -15,7 +15,10 @@ public class TutorialContractFrames {
     public static void main(String[] args) {
         
         // Input text goes in this variable txt
-        String txt = "A established a purchase contract with B to buy a land L, on April 1, 2018. A rescinded the purchase contract by a fraud by the thrid party C on May 1, 2018. However, the recission was actually made due to B's duress on April 15, 2018 since B would like to send the land to D because D said that D pays more to purched the land than A pays. A made a rescission of the above rescission on June 1, 2018.";
+        //String txt = "A established a purchase contract with B to buy a land L, on April 1, 2018. A rescinded the purchase contract by a fraud by the thrid party C on May 1, 2018. However, the recission was actually made due to B's duress on April 15, 2018 since B would like to send the land to D because D said that D pays more to purched the land than A pays. A made a rescission of the above rescission on June 1, 2018.";
+
+        String txt = "The contract establishes that Maria purchases a car from Victor. Maria is under 18.";
+
         
         ContractFrames cf = new ContractFrames();
         String output = cf.annotate(txt);
